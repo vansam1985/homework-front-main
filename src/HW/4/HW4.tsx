52fd05a304f58515e4127755446118028d367331
Ivan Samoylov <vansam1985@yandex.ru>
2023-11-25 12:07:40 +0300

    4

--- src/HW/4/HW4.tsx
+++ src/HW/4/HW4.tsx
@@ -32,7 +32,7 @@ export const HW4 = () => {
 
       <Input currentText={currentText} setCurrentText={setCurrentText} />
 
-      <Button name={'SAVE'} callBack={}/>
+      <Button name={'SAVE'} callBack={handleSave}/>
 
       <h1 style={{ marginTop: '50px' }}>СПИСОК ДЕЛ НА ДЕНЬ:</h1>
 
